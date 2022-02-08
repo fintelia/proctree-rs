e8753307c9a71605b7b18563765f2b570cc3cd41
Jonathan Behrens <fintelia@gmail.com>
2022-02-07 19:10:46 -0500

    Fix build.rs

--- proctree-sys/build.rs
+++ proctree-sys/build.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("cargo:rustc-link-lib=dylib=stdc++");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=wrapper.hpp");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
